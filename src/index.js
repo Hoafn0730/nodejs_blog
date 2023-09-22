9a172019d26a043727052d43c9f54f4936feec4d
Hoafn0730 <hoantran0730@gmail.com>
2023-09-22 20:42:07 +0700

    oke

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const path = require('path');
+const handlebars = require('express-handlebars');
+
 const app = express();
 const port = 3000;
 
+// Use static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 });
+
+// app.post('/post', (req, res, next) => {});
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
